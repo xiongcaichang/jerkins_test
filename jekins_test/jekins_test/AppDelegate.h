1ebfd0c50bf9b17364233f9d1df5c08c4a9e4b2e
Bear <xiongcc@xiaoshouyi.com>
2016-10-31 14:50:07 +0800

    no message

---
+++ jekins_test/jekins_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jekins_test
+//
+//  Created by bear on 16/10/31.
+//  Copyright © 2016年 bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jekins_test/jekins_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jekins_test
+//
+//  Created by bear on 16/10/31.
+//  Copyright © 2016年 bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
