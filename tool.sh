ee559c5b2297ba6908cc78d2cecc6a2d0431f5d7
Bear <xiongcc@xiaoshouyi.com>
2016-11-01 13:33:13 +0800

    no message

--- tool.sh
+++ tool.sh
@@ -4,6 +4,8 @@
 import smtplib  
 import sys
 import os
+import time
+
 
 
 from email.mime.text import MIMEText  
@@ -13,13 +15,22 @@ mail_user="17777785303"    #用户名
 mail_pass="qq123456"   #口令 
 mail_postfix="163.com"  #发件箱的后缀
 
-WORKSPACE =sys.argv[1]
+workspace =sys.argv[1]
 
-command_upload_to_fir = "fir p ${WORKSPACE}/build/jekins_test.ipa -T 3b501039782b9931cb4de6c4a0f82ce9"
+command_upload_to_fir = "fir p ${workspace}/build/jekins_test.ipa -T 3b501039782b9931cb4de6c4a0f82ce9"
 
-comRS = os.popen(command_upload_to_fir)
+cmdrs = os.popen(command_upload_to_fir)
 
-MAILCONTENT = comRS.read()
+cmdrs_str = cmdrs.read()
+
+
+def getUrlFromStr(content):
+    regex = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", re.IGNORECASE)
+    urls = regex.findall(fobj)
+    print urls
+    return urls
+
+
   
 def send_mail(to_list,sub,content):  #to_list：收件人；sub：主题；content：邮件内容
     me="hello"+"<"+mail_user+"@"+mail_postfix+">"   #这里的hello可以任意设置，收到信后，将按照设置显示
@@ -37,8 +48,11 @@ def send_mail(to_list,sub,content):  #to_list：收件人；sub：主题；conte
     except Exception, e:  
         print str(e)  
         return False  
-if __name__ == '__main__':  
-    if send_mail(mailto_list,"成功打包",MAILCONTENT):  
+if __name__ == '__main__':
+
+    app_url =  getUrlFromStr(cmdrs_str)
+
+    if send_mail(mailto_list,"测试app下载地址 打包时间:"+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),{`<h3><a href=${app_url}>${app_url}</h3>`}):  
         print "发送成功"  
     else:  
-        print "发送失败"+ "<"+mail_user+"@"+mail_postfix+">"
+        print "发送失败"
